3dbc2df2226dec27982af152b54e453bbbdffad6
GSevensT <143946090+GSevensT@users.noreply.github.com>
2024-10-09 22:47:56 +0800

    update and removal of original APP

---
+++ src/components/Themes/Themes.jsx
@@ -0,0 +1,47 @@
+export const theme = {
+    colors: {
+      black: '#212121',
+      gray: '#9B9FAA',
+      orange: '#FC842D',
+      blue: '#264061',
+      backgroundGray: '#F0F1F3',
+      white: '#ffffff',
+      hover: '#f07116',
+      filter: 'none',
+    },
+    space: [0, 4, 8, 16, 20, 32, 64, 128, 256, 512].map(n => n + 'px'),
+    fontSizes: [14, 18, 24, 34, 36].map(n => n + 'px'),
+    lineHeights: {
+      body: 1.2,
+      heading: 1.125,
+    },
+    borders: {
+      none: 'none',
+      normal: '1px solid #E0E0E0',
+      header: '1px solid #E0E0E0',
+      btn: '2px solid #FC842D',
+      hover: '2px solid #f07116',
+    },
+    btnBefore: {
+      position: 'none',
+      top: 'none',
+      left: 'none',
+      width: 'none',
+      height: 'none',
+      background: 'none',
+      borderRadius: 'none',
+      boxShadow: 'none',
+    },
+    btnAfter: {
+      position: 'none',
+      top: 'none',
+      left: 'none',
+      width: 'none',
+      height: 'none',
+      background: 'none',
+      borderRadius: 'none',
+      boxShadow: 'none',
+    },
+  };
+  
+  
