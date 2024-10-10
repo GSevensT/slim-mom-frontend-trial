2572d6cdf7b7e48391da5d4575b84ab99fd1a7cf
GSevensT <143946090+GSevensT@users.noreply.github.com>
2024-10-10 16:07:06 +0800

    add imports request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,43 @@
 
-export const App = () => {
 
+//add imports 
+
+export const App = () => {
+    const dailyRate = useSelector(state => state.auth.userInfo.dailyRate);
+
+    const dispatch = useDispatch();
+    const token = useSelector(getToken);
+  
+    const mockQuery = '';
+    const { data } = useGetUserQuery(mockQuery, { skip: !token });
+  
+    useEffect(() => {
+      if (!data) {
+        return;
+      }
+      if (dailyRate) {
+        return;
+      }
+  
+      dispatch(setCurrentUser(data));
+    }, [dailyRate, data, dispatch]);
+
+    return (
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route
+                  path="diary"
+                  element={
+                    <PrivateRoute>
+                      <Diary />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="/*" element={<NotFound />} />
+              </Route>
+            </Routes>
+          </Suspense>
+      );
+      
 };
