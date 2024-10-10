1b0385de7b2308dc23f9747e2eaa1e0e8080c101
GSevensT <143946090+GSevensT@users.noreply.github.com>
2024-10-10 16:31:13 +0800

    update

--- src/components/Routes/PublicRoute.js
+++ src/components/Routes/PublicRoute.js
@@ -1,9 +1,9 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { getIsLoggedIn } from "redux/authSelectors";
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { getIsLoggedIn } from 'redux/authSelectors';
 
-export const PublicRoute = ({ Children, restricted = false }) => {
-    const IsLoggedIn = useSelector(getIsLoggedIn);
-    const shouldRedirect = isLoggedIn && restricted;
-    return shouldRedirect ? <Navigate to="/diary" /> : children;
+export const PublicRoute = ({ children, restricted = false }) => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const shouldRedirect = isLoggedIn && restricted;
+  return shouldRedirect ? <Navigate to="/diary" /> : children;
 };
